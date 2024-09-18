68c0a6f87bb71829f7aef25445ab7584be3f649d
AzizSaydullayev <azizbeksaydullayv66@gmail.com>
2024-09-18 22:09:06 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let ism = prompt ("Ismingizni kiriting")
+let id = +prompt("Id kiriting")
+let password = +prompt("Password kiriting")
+
+
+let person = {
+firstName:ism,
+Id:id,
+password:password,
+}
+
+console.log(person);
